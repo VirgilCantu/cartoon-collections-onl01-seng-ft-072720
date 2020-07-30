dd6622d3f7cd0be9c3237ebb27ba47be36cde18f
Virgil Cantu <cantu.sampsom@gmail.com>
2020-07-30 17:52:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.find {|cheese| strings.include?(cheese)}
+  strings.find {|ingredient| strings.include?(cheese_types)}
 end
 
